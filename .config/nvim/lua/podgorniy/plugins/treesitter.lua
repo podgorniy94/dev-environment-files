b8b3b77e012e16709c1cf92eba0b791fbd4dc6f7
Rostislav Podgorniy <podgorniyr@mobatime.cz>
2026-04-26 01:45:04 +0200

    Update treesitter config for Neovim 0.12

--- .config/nvim/lua/podgorniy/plugins/treesitter.lua
+++ .config/nvim/lua/podgorniy/plugins/treesitter.lua
@@ -1,57 +1,48 @@
 return {
+  -- Tree-sitter parsers and queries for Neovim
   'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPre', 'BufNewFile' },
-  build = ':TSUpdate',
-  dependencies = {
-    'windwp/nvim-ts-autotag',
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require('nvim-treesitter.configs')
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        'json',
-        'javascript',
-        'typescript',
-        'tsx',
-        'yaml',
-        'html',
-        'css',
-        'markdown',
-        'markdown_inline',
-        'svelte',
-        'bash',
-        'lua',
-        'vim',
-        'dockerfile',
-        'gitignore',
-        'query',
-        'vimdoc',
-        'c',
-        'python',
-        'jinja',
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
-        },
-      },
+  branch = 'main',
+
+  -- New nvim-treesitter main branch should not be lazy-loaded
+  lazy = false,
+
+  -- Update installed parsers after plugin update
+  build = ':TSUpdate',
+
+  config = function()
+    -- Install parsers for the languages/filetypes you use
+    require('nvim-treesitter').install({
+      'json',
+      'javascript',
+      'typescript',
+      'tsx',
+      'yaml',
+      'html',
+      'css',
+      'markdown',
+      'markdown_inline',
+      'svelte',
+      'bash',
+      'lua',
+      'vim',
+      'dockerfile',
+      'gitignore',
+      'query',
+      'vimdoc',
+      'c',
+      'python',
+      'jinja',
+    })
+
+    -- Use the bash parser for zsh files
+    vim.treesitter.language.register('bash', 'zsh')
+
+    -- Start Tree-sitter highlighting automatically when a parser exists
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
     })
   end,
 }
